95333b0ea1469abfe76dc941faeff466698e9fb5
Bhoma ram <bhomaram@Bhomas-Mac-mini.local>
2022-02-22 01:30:20 +0530

    add file added

---
+++ contact.js
@@ -0,0 +1,10 @@
+const add = (first, second) =>{
+    return first + second;
+}
+
+const runGit = () =>{
+    console.log("RunningGit-->");
+    console.log("Addition of 10 + 10 = ", add(10, 10));
+}
+
+runGit();
