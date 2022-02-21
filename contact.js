454e17de6ed21246912adc72eb95e915f72c6251
Bhoma ram <bhomaram@Bhomas-Mac-mini.local>
2022-02-22 01:42:40 +0530

    sub added

--- contact.js
+++ contact.js
@@ -2,9 +2,14 @@ const add = (first, second) =>{
     return first + second;
 }
 
+const sub = (first, second) => {
+    return first - second;
+}
+
 const runGit = () =>{
     console.log("RunningGit-->");
     console.log("Addition of 10 + 10 = ", add(10, 10));
+    console.log("Subtraction of 10 - 5", sub(10, 5));
 }
 
 runGit();
