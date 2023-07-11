9fa0a4c8ba36cfe6fe1f01cb2f9b1afc02a15ff7
hennadii919 <yenenkohennadii919@gmail.com>
2023-07-12 00:01:12 +0530

    Registration page created

---
+++ register.ts
@@ -0,0 +1,8 @@
+function registerUser(name: string, phone: string, email: string){
+  console.log("Name: ", name);
+  console.log("Phone: ", phone);
+  console.log("Email: ", email);
+}
+
+
+registerUser("Bhomaram", "+918875297979", "br.bhomaram@omg.com");
